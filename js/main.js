d7226f6b781460c70a4ca7b61aead97d6bb4b03b
kazuya fujiwara <kazuyafujiwara@kazuyanoMacBook-Pro.local>
2022-09-19 03:03:23 +0900

    js edited but not working

--- js/main.js
+++ js/main.js
@@ -2,3 +2,21 @@ var scroll = new SmoothScroll('a[href*="#"]', {
   speed: 500,
   speedAsDuration: true,
 });
+
+jQuery(window).on("scroll", function ($) {
+  if (jQuery(this).scrollTop() > 100) {
+    jQuery(".floating").show();
+  } else {
+    jQuery(".floating").hide();
+  }
+});
+
+jQuery(".floating").click(function () {
+  jQuery("body, html").animate(
+    {
+      scrollTop: 0,
+    },
+    500
+  );
+  return false;
+});
